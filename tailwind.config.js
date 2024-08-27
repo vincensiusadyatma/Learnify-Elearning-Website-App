42d34247758941e7e52111dda5ec5ce1ec3193f8
vincensiusadyatma <adyatmav@gmail.com>
2024-08-27 12:28:20 +0700

    feat: add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [ 
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",],
+  theme: {
+    screens: {
+      'sm': '340px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '568px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {},
+  },
+  darkMode: 'class',
+  plugins: [],
+}
+
